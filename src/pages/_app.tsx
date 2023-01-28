2be516d8a12f24033142111d126da7dba24ca364
nodgear <matheus.silva3@ftc.edu.br>
2023-01-28 15:00:25 -0300

    chore: import tailwind to globals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 
-import "../style.css";
-import "../App.css";
+import "@/styles/globals.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
