2863254bca7692f6d351e71780b53ce1463a0be4
nodgear <matheus.silva3@ftc.edu.br>
2023-01-28 22:10:41 -0800

    feat: Settings applet

---
+++ src/components/Applets/Settings/Navbar.tsx
@@ -0,0 +1,15 @@
+import { Gear } from 'phosphor-react'
+import NavIcon from '@/components/Navbar/NavbarIcon';
+
+type Props = {}
+
+function Navbar({}: Props) {
+  return  <NavIcon 
+    title='Settings'  
+    Icon={<Gear size={22}/>} 
+    onClick={()=>{
+  }}
+/>
+}
+
+export default Navbar
