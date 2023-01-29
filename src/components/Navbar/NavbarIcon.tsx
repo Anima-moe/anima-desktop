fd48893cafa4080f9d8269fbbc1acb00595ee83b
nodgear <matheus.silva3@ftc.edu.br>
2023-01-29 06:49:26 -0300

    style: change navicon colors

--- src/components/Navbar/NavbarIcon.tsx
+++ src/components/Navbar/NavbarIcon.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 function NavbarIcon({title, Icon, onClick}: Props) {
   return (
-    <div className='h-full aspect-square bg-secondary border border-tertiary ml-3 rounded-md flex items-center justify-center p-2 text-subtle hover:text-accent hover:bg-black duration-300 hover:border-accent cursor-pointer'>
+    <div className='h-full aspect-square bg-secondary border border-tertiary ml-3 rounded-md flex items-center justify-center p-2 text-subtle hover:text-white hover:bg-black duration-300 hover:border-tertiary cursor-pointer active:bg-accent active:border-accent active:text-primary'>
       {Icon}
     </div>
   )
