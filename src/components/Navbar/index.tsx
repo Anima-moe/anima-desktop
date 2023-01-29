bd3db5bf31b9eabf511919e9e133797d9c65022f
nodgear <matheus.silva3@ftc.edu.br>
2023-01-28 22:10:52 -0800

    feat: go home when clicking logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,15 +3,20 @@ import { motion } from 'framer-motion'
 import SearchApplet from '@/components/Applets/Search/Navbar'
 import NotificationApplet from '@/components/Applets/Notifications/Navbar'
 import UserApplet from '@/components/Applets/User/Navbar'
+import ConfigApplet from '@/components/Applets/Settings/Navbar'
+import { useRouter } from 'next/router'
 
 
 function Index() {
- 
+  const router = useRouter()
+
   return <motion.nav 
     className='w-full py-3 px-8 flex flex-row select-none h-16 fixed z-[99]'
   >
     {/* LOGO */}
-    <div className='flex w-3/12 items-center'>
+    <div className='flex w-3/12 items-center cursor-pointer' onClick={()=>{
+      router.push('/')
+    }}>
       <img src='/anima.svg' className='w-22' />
     </div>
     {/* SEARCH BAR */}
@@ -21,6 +26,7 @@ function Index() {
     {/* NAVBAR ICONS */}
     <div className='flex w-3/12 items-center justify-end'>
         <NotificationApplet />
+        <ConfigApplet />
         <UserApplet />
     </div>
   </motion.nav>
