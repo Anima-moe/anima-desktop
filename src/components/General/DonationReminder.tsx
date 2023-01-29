46013dfdc5b140b3ccb43b30a64e60aef5e88a64
nodgear <matheus.silva3@ftc.edu.br>
2023-01-28 21:11:47 -0800

    style: prevent user selection

--- src/components/General/DonationReminder.tsx
+++ src/components/General/DonationReminder.tsx
@@ -7,7 +7,7 @@ type Props = {}
 function DonationReminder({}: Props) {
   const [isVisible, setIsVisible] = useState(true)
   return isVisible ? (
-    <div className="w-full bg-accent rounded-md text-primary px-4 py-2 text-xs font-semibold justify-between flex-row items-center flex">
+    <div className="w-full bg-accent rounded-md text-primary px-4 py-2 text-xs font-semibold justify-between flex-row items-center flex select-none">
       <span>Hey, listen! @ Anima.moe we will never force ads on you, we survive on donations. Please consider donating to keep the service running ad-free 💞</span>
       <X size={16} onClick={()=> { setIsVisible(false) } } className='cursor-pointer' />
     </div> 
