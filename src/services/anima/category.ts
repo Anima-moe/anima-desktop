09927a80b4220135858ef3b3983d95477d0959ad
nodgear <matheus.silva3@ftc.edu.br>
2023-01-30 02:41:36 -0300

    feat: category service start

---
+++ src/services/anima/category.ts
@@ -0,0 +1,15 @@
+import client from '@/services/anima/httpService'
+import { Anime } from './anime'
+
+export class Episode {
+
+  get = async function (id: number) {
+
+  }
+
+  getBySlug = async function (slug: string) {
+
+  }
+
+  getAm
+}
