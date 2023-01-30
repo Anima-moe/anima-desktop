554ad5b6af3479d6d0d1dbc86155941657be1b63
nodgear <matheus.silva3@ftc.edu.br>
2023-01-30 13:13:30 -0300

    refactor: use phosphor for pills

--- src/components/General/Pill.tsx
+++ src/components/General/Pill.tsx
@@ -1,16 +1,16 @@
 import type { ReactNode } from 'react'
-import type { IconType } from 'react-icons'
+import type { IconProps } from 'phosphor-react'
 
 type Props = {
-  Icon: IconType,
+  Icon: React.ForwardRefExoticComponent<IconProps & React.RefAttributes<SVGSVGElement>>,
   children: ReactNode,
   color?: string
 }
 
 function Pill({Icon, children, color}: Props) {
   return (
-    <div className="bg-background-blur backdrop-blur-md border border-border-blur border-opacity-20 flex items-center rounded-full bg-opacity-30 px-2 text-sm mr-3" style={{color}}>
-      <Icon  className="mr-2"/> {children}
+    <div className="bg-secondary border border-tertiary flex items-center rounded-full px-3 py-1 text-sm mr-3" style={{color}}>
+      <Icon className="mr-2" weight='fill'/> {children}
     </div>
   )
 }
