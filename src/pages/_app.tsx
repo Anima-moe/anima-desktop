e0f9c4bc81a23e181c92cb13139e06cfe124904f
nodgear <matheus.silva3@ftc.edu.br>
2023-01-31 06:35:32 -0300

    feat: nloader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,13 @@ import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import type { AppProps } from "next/app"
 import languageTable from '@/services/i18n/languageTable'
-
-import "@/styles/globals.css"
+import NProgress from 'nprogress'
 import { useEffect } from "react"
 import { invoke } from "@tauri-apps/api/tauri"
+import { useRouter } from "next/router"
+
+import "@/styles/globals.css"
+import "nprogress/nprogress.css"
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
@@ -17,8 +20,38 @@ i18n
     }
   })
 
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'easeInOut',
+  speed: 300,
+  showSpinner: true,
+})
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start()
+    }
+
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
+
   useEffect(()=>{
     (async ()=>{
       const { getConfigValue } = await import('@/services/tauri/configValue')
@@ -27,21 +60,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
       const { appWindow } = await import('@tauri-apps/api/window')
 
-      if (!await appWindow.isVisible()) { return }
-      await invoke('discord_set_activity', {
-        details: 'Assistindo',
-        state: 'S1 E1 - Anima test episode',
-        timestamp: Date.now(),
-        image: 'logo'
-      })  
-      // discord_set_activity(
-      //   details: &str,
-      //   state: &str,
-      //   timestamp: i64,
-      //   image: &str,
-      //   client: State<'_, DeclarativeDiscordIpcClient>
-      // )
+      if (!await appWindow.isVisible()) { return } 
     })()
   }, [])
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
