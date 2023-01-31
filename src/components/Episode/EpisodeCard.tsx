78ff627b69dab87ded2e8aaf705475c290c011d3
nodgear <matheus.silva3@ftc.edu.br>
2023-01-31 15:19:44 -0300

    fix: generic types metadata getter

--- src/components/Episode/EpisodeCard.tsx
+++ src/components/Episode/EpisodeCard.tsx
@@ -17,10 +17,10 @@ function EpisodeCard({episode}: Props) {
         <span className='bg-primary text-accent px-2 rounded-md mr-1.5 flex items-center justify-center'>
           {beautyNumber(episode.number) || '?'}
         </span>
-        {getLocaleMetadata(episode)?.title || 'Unknown title'}
+        {getLocaleMetadata<Anima.RAW.Episode, Anima.RAW.EpisodeMetadata>(episode)?.title || 'Unknown title'}
       </h2>
       <p className='text-sm text-white text-opacity-60 line-clamp-5 group-hover:text-secondary'>
-        {getLocaleMetadata(episode)?.synopsis || 'Missing synopsis'}
+        {getLocaleMetadata<Anima.RAW.Episode, Anima.RAW.EpisodeMetadata>(episode)?.synopsis || 'Missing synopsis'}
       </p>
     </div>
   </div>
