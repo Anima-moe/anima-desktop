990bcfd0f7501933d89a3c981af4a54a8deae8d2
Matheus Alves <matheus.silva3@ftc.edu.br>
2023-02-09 05:43:43 -0300

    fix: remove unused locale param from seasons srvce

--- src/services/anima/season.ts
+++ src/services/anima/season.ts
@@ -2,9 +2,7 @@ import client from '@/services/anima/httpService'
 
 export const Season = {
   get: async (id: number, locale: string) => {
-    const { data } = await client.get(`/season/${id}`, {
-      locale
-    })
+    const { data } = await client.get(`/season/${id}`, {})
     return data as Anima.API.GetSeasonByID
   },
 }
