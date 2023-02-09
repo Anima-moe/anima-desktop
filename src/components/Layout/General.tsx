1b2c2ee88db11cdfd9cb376726f761fd21ec3474
Matheus Alves <matheus.silva3@ftc.edu.br>
2023-02-09 05:44:27 -0300

    refactor: stop using custom scrollbar on layout

--- src/components/Layout/General.tsx
+++ src/components/Layout/General.tsx
@@ -10,9 +10,7 @@ function General({children, fluid}: Props) {
   return <>
     <Navbar />
     <main className={`h-screen flex flex-col ${fluid ? 'px-0 w-full' : 'px-8'}`}>
-      <Scrollbars autoHide hideTracksWhenNotNeeded universal>
         {children}
-      </Scrollbars>
     </main>
   </>
 }
