7107bbafc82c640bd8d04b20f850b8960b92229c
Matheus Alves <matheus.silva3@ftc.edu.br>
2023-02-09 06:20:16 -0300

    fix: prevent player from crashing without subs

--- src/components/Player/subtitleController.tsx
+++ src/components/Player/subtitleController.tsx
@@ -15,20 +15,13 @@ export default class SubtitleController {
     this._media = media
     this._streamData = streamData
 
-    console.log("Subscribing to media event -> Play -> Render subtitle / Replace track URL")
-
-
-    console.log("Media player is playing, initializing subtitle controller")
-
     const opiniatedSubtitle = this.getOpiniatedSubtitle()
     console.log("User opiniated subtitle", this._streamData.subtitles[opiniatedSubtitle])
 
     if (!this._renderer) {
-      console.log("JASSUB is not initialized, initializing JASSUB")
       import('jassub').then(JASSUB => {
         this.initialize(JASSUB)
-        if (!opiniatedSubtitle || this._streamData.subtitles[opiniatedSubtitle].url === '') {
-          console.log("No subtitle available / user disabled subtitle")
+        if (!opiniatedSubtitle || this._streamData.subtitles[opiniatedSubtitle]?.url === '') {
           return
         }
 
@@ -41,8 +34,6 @@ export default class SubtitleController {
   }
 
   private initialize(JASSUB) {
-    console.log("JASSUB")
-
     if (!this._renderer) {
       this._renderer = new JASSUB.default({
         // video: this._media.querySelector('video'),
@@ -57,10 +48,6 @@ export default class SubtitleController {
         console.warn("PROVIDER CHANGED", this._media.querySelector('video'))
         this._renderer.setVideo(this._media.querySelector('video'))
       })
-
-      console.log("RENDERER", this._renderer)
-    } else {
-      console.log("OLD RENDERER", this._renderer)
     }
   }
 
@@ -74,30 +61,23 @@ export default class SubtitleController {
     if (!availableSubs) return
     if (userPrefered === '' || !userPrefered || !userEnabledSubtitles) { return '' }
 
-    console.log("User has subs enabled, it's no an empty string and there's available subs")
 
     if (Object.keys(availableSubs).length === 1) {
       return availableSubs[Object.keys(availableSubs)[0]].locale
     }
 
-    console.log("There's more than one subtitle available")
     console.log("FILTER:", Object.keys(availableSubs).filter(locale => locale === userPrefered)[0])
     return Object.keys(availableSubs).filter(locale => locale === userPrefered)[0]
   }
 
   public requestSubtitleChange(locale: string) {
-    console.log('Requesting subtitle change to', locale, this._renderer)
     if (!this._renderer) { return }
-    console.log("Available subtitles:", this._streamData.subtitles)
-    if (this.currentSubtitle === this._streamData.subtitles[locale].url) {
-      console.log("Same subtitle, skipping")
+    if (this._streamData.subtitles[locale] && this.currentSubtitle === this._streamData.subtitles[locale].url) {
       return
     }
     
-    console.log("Subtitle is not the same as before, proceeding with change procedures")
 
     if (locale === '' && this.currentSubtitle || !locale) {
-      console.log("Apparently requested to change subtitles to empty string?")
       this.currentSubtitle = ''
       this.currentSubtitleLocale = ''
       writeAtom(playerStreamConfig, {
@@ -111,11 +91,9 @@ export default class SubtitleController {
       return
     }
 
-    console.log("Requesting jassub track change!")
-    this.currentSubtitle = this._streamData.subtitles[locale].url
+    this.currentSubtitle = this._streamData.subtitles[locale]?.url || ''
     this.currentSubtitleLocale = locale
     this._renderer.setTrackByUrl(this.currentSubtitle)
-    console.log("JASSUB track changed!", this._media.querySelector('video'), this._renderer)
     writeAtom(userPreferedSubtitles, locale)
     writeAtom(playerStreamConfig, {
       ...readAtom(playerStreamConfig),
