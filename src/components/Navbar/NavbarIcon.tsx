41d497649a0771973bcb576ea02ec1e2abb6d8c1
GabrielM <profissional.gabrielm@gmail.com>
2023-02-13 19:54:53 -0300

    fix: Navbar spacing

--- src/components/Navbar/NavbarIcon.tsx
+++ src/components/Navbar/NavbarIcon.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 function NavbarIcon({title, Icon, onClick}: Props) {
   return (
-    <div className='h-full aspect-square bg-secondary border border-tertiary ml-3 rounded-md flex items-center justify-center p-2 text-subtle hover:text-white hover:bg-black duration-300 hover:border-tertiary cursor-pointer active:bg-accent active:border-accent active:text-primary bg-opacity-60 backdrop-blur-sm border-opacity-50 '>
+    <div className='h-full aspect-square bg-secondary border border-tertiary rounded-md flex items-center justify-center p-2 text-subtle hover:text-white hover:bg-black duration-300 hover:border-tertiary cursor-pointer active:bg-accent active:border-accent active:text-primary bg-opacity-60 backdrop-blur-sm border-opacity-50'>
       {Icon}
     </div>
   )
