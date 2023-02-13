f691cc5e71260d24ec5ca17c95113fde29687383
GabrielM <profissional.gabrielm@gmail.com>
2023-02-13 19:56:00 -0300

    feat: user drop down menu

--- src/components/Applets/User/Navbar.tsx
+++ src/components/Applets/User/Navbar.tsx
@@ -1,16 +1,42 @@
-import { User } from 'phosphor-react'
+import { SignOut, User } from 'phosphor-react'
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/General/DropdownMenu'
 import NavIcon from '@/components/Navbar/NavbarIcon'
 
 function Navbar() {
-  return <>
-  <NavIcon 
-    title='No notifications'  
-    Icon={<User size={22}/>} 
-    onClick={()=>{
-    }}
-  />
-</>
+  const handleLogout = () => {}
+
+  const menuItems = [{ name: 'Sign out', Icon: SignOut, click: handleLogout }]
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <NavIcon title="No notifications" Icon={<User size={22} />} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade min-w-[220px] rounded-md bg-secondary p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform]"
+        sideOffset={5}
+      >
+        {menuItems.map((item) => (
+          <DropdownMenuItem
+            key={item.name}
+            className="relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-black data-[disabled]:text-gray-600 data-[highlighted]:text-white"
+            onClick={item.click}
+          >
+            <div className="left-0 inline-flex w-6 items-center justify-center">
+              <item.Icon />
+            </div>
+            {item.name}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
 
 export default Navbar
