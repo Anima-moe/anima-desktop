1a39864dc3e05272c168a643e6da28da21174056
Nodge <matheus.silva3@ftc.edu.br>
2023-02-13 21:03:39 -0300

    fix: dropdown shit

--- src/components/Applets/User/Navbar.tsx
+++ src/components/Applets/User/Navbar.tsx
@@ -1,12 +1,7 @@
-import { SignOut, User } from 'phosphor-react'
+import { Gear, SignOut, User } from 'phosphor-react'
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/General/DropdownMenu'
 import NavIcon from '@/components/Navbar/NavbarIcon'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
 function Navbar() {
   const handleLogout = () => {}
@@ -14,28 +9,37 @@ function Navbar() {
   const menuItems = [{ name: 'Sign out', Icon: SignOut, click: handleLogout }]
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <NavIcon title="No notifications" Icon={<User size={22} />} />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        className="data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade min-w-[220px] rounded-md bg-secondary p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform]"
-        sideOffset={5}
-      >
-        {menuItems.map((item) => (
-          <DropdownMenuItem
-            key={item.name}
-            className="relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-black data-[disabled]:text-gray-600 data-[highlighted]:text-white"
-            onClick={item.click}
-          >
-            <div className="left-0 inline-flex w-6 items-center justify-center">
-              <item.Icon />
-            </div>
-            {item.name}
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className=''>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button 
+            className='h-full aspect-square bg-secondary border border-tertiary rounded-md flex items-center justify-center p-2 text-subtle hover:text-white hover:bg-black duration-300 hover:border-tertiary cursor-pointer active:bg-accent active:border-accent active:text-primary bg-opacity-60 backdrop-blur-sm border-opacity-50'
+            // onClick={()=>{
+            // }}
+        >
+          <User size={22}/>
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className="bg-secondary border border-tertiary min-w-[13rem] px-1 py-2 rounded-md mr-8" sideOffset={5}>
+          <DropdownMenu.Item className="rounded-md hover:bg-accent bg-secondary duration-200 transition-all cursor-pointer px-2 py-2 hover:text-primary my-1">
+            <span className='flex '><User size={22} className='mr-3'/> My profile</span>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="rounded-md hover:bg-accent bg-secondary duration-200 transition-all cursor-pointer px-2 py-2 hover:text-primary  my-1">
+            <span className='flex '><Gear size={22} className='mr-3'/> Settings</span>
+          </DropdownMenu.Item>
+          <DropdownMenu.Separator className="w-full border border-tertiary  my-1" />
+          <DropdownMenu.Item className="rounded-md hover:bg-red-400 bg-secondary duration-200 transition-all cursor-pointer px-2 py-2 hover:text-primary mt-1">
+            <span className='flex '><SignOut size={22} className='mr-3'/> Sign out</span>
+           </DropdownMenu.Item>
+          <DropdownMenu.Arrow className="DropdownMenuArrow" />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+
+
+    </div>
   )
 }
 
