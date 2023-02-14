24f486ed81dc0b2ffc02bd0b9dcb2b7432b81972
GabrielMar <profissional.gabrielm@gmail.com>
2023-02-14 04:04:26 -0300

    fix: rewrite component

--- src/components/Applets/User/Navbar.tsx
+++ src/components/Applets/User/Navbar.tsx
@@ -1,44 +1,69 @@
+import { useTranslation } from 'react-i18next'
+
+import clsx from 'clsx'
+import { useRouter } from 'next/router'
 import { Gear, SignOut, User } from 'phosphor-react'
 
-import NavIcon from '@/components/Navbar/NavbarIcon'
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/General/DropdownMenu'
 
 function Navbar() {
+  const router = useRouter()
+  const { t } = useTranslation()
+
+  const handleProfile = () => {
+    router.push(`/user/${1}`)
+  }
+  const handleSettings = () => {
+    router.push(`/user/${1}/edit`)
+  }
   const handleLogout = () => {}
 
-  const menuItems = [{ name: 'Sign out', Icon: SignOut, click: handleLogout }]
+  const menuItems = [
+    { name: t('user_menu_profile'), Icon: User, click: handleProfile },
+    { name: t('user_menu_settings'), Icon: Gear, click: handleSettings },
+    { name: t('user_menu_signout'), Icon: SignOut, click: handleLogout, class: 'hover:bg-red-400' },
+  ]
 
   return (
-    <div className=''>
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild>
-        <button 
-            className='h-full aspect-square bg-secondary border border-tertiary rounded-md flex items-center justify-center p-2 text-subtle hover:text-white hover:bg-black duration-300 hover:border-tertiary cursor-pointer active:bg-accent active:border-accent active:text-primary bg-opacity-60 backdrop-blur-sm border-opacity-50'
-            // onClick={()=>{
-            // }}
+    <div className="">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button className="flex aspect-square h-full cursor-pointer items-center justify-center rounded-md border border-tertiary border-opacity-50 bg-secondary bg-opacity-60 p-2 text-subtle backdrop-blur-sm duration-300 hover:border-tertiary hover:bg-black hover:text-white active:border-accent active:bg-accent active:text-primary">
+            <User size={22} />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className="z-50 min-w-[13rem] rounded-md border border-tertiary bg-secondary px-2 py-2"
+          sideOffset={16}
+          align="end"
         >
-          <User size={22}/>
-        </button>
-      </DropdownMenu.Trigger>
-
-      <DropdownMenu.Portal>
-        <DropdownMenu.Content className="bg-secondary border border-tertiary min-w-[13rem] px-1 py-2 rounded-md mr-8" sideOffset={5}>
-          <DropdownMenu.Item className="rounded-md hover:bg-accent bg-secondary duration-200 transition-all cursor-pointer px-2 py-2 hover:text-primary my-1">
-            <span className='flex '><User size={22} className='mr-3'/> My profile</span>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item className="rounded-md hover:bg-accent bg-secondary duration-200 transition-all cursor-pointer px-2 py-2 hover:text-primary  my-1">
-            <span className='flex '><Gear size={22} className='mr-3'/> Settings</span>
-          </DropdownMenu.Item>
-          <DropdownMenu.Separator className="w-full border border-tertiary  my-1" />
-          <DropdownMenu.Item className="rounded-md hover:bg-red-400 bg-secondary duration-200 transition-all cursor-pointer px-2 py-2 hover:text-primary mt-1">
-            <span className='flex '><SignOut size={22} className='mr-3'/> Sign out</span>
-           </DropdownMenu.Item>
-          <DropdownMenu.Arrow className="DropdownMenuArrow" />
-        </DropdownMenu.Content>
-      </DropdownMenu.Portal>
-    </DropdownMenu.Root>
-
-
+          {menuItems.map((item, i, arr) => (
+            <>
+              {i === arr.length - 1 && (
+                <DropdownMenuSeparator className="my-2 w-full border border-tertiary" />
+              )}
+              <DropdownMenuItem
+                className={clsx(
+                  'cursor-pointer rounded-md bg-secondary px-2 py-2 transition-all duration-200 hover:bg-accent hover:text-primary',
+                  item.class
+                )}
+                onClick={item.click}
+              >
+                <span className="flex">
+                  <item.Icon size={22} className="mr-3" />
+                  {item.name}
+                </span>
+              </DropdownMenuItem>
+            </>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   )
 }
