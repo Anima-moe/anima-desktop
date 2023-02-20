8c3ff072f81a0a632bb39f0bf802f450996eece2
Nodge <matheus.silva3@ftc.edu.br>
2023-02-20 07:20:39 -0300

    fix: apply profile color to components

--- src/components/User/UserCard.tsx
+++ src/components/User/UserCard.tsx
@@ -28,7 +28,7 @@ const UserCard = ({ showStatics, user }: Props) => {
   return (
     <>
       <div
-        className="h-48 rounded-t-md bg-tertiary bg-cover bg-center bg-no-repeat relative"
+        className='h-48 rounded-t-md bg-tertiary bg-cover bg-center bg-no-repeat relative'
         style={{
           backgroundImage: `url(${user?.profile?.background})`,
         }}
@@ -38,16 +38,16 @@ const UserCard = ({ showStatics, user }: Props) => {
           <Button secondary Icon={<NotePencil className='mr-3' />} text={t('user_menu_settings')} iconLeft xs/>
         </div>
       </div>
-      <div className="relative mb-4 flex h-24 justify-between overflow-hidden rounded-b-md bg-secondary px-8">
-        <div className="pointer-events-none absolute top-0 left-0 z-[0] h-full w-full bg-accent/10" />
-        <div className="z-[1] flex items-center">
+      <div className='relative mb-4 flex h-24 justify-between overflow-hidden rounded-b-md bg-secondary px-8'>
+        <div className='pointer-events-none absolute top-0 left-0 z-[0] h-full w-full opacity-10' style={{backgroundColor: `${user?.profile?.color || '#161616'}`}}/>
+        <div className='z-[1] flex items-center'>
           <span
-            className="mr-4 h-20 w-20 rounded-full bg-cover bg-center bg-no-repeat"
+            className='mr-4 h-20 w-20 rounded-full bg-cover bg-center bg-no-repeat'
             style={{ backgroundImage: 'url(/test/avtr.png)' }}
           />
-          <div className="flex flex-col">
-            <div className="flex items-center gap-x-3">
-              <span className="text-2xl font-semibold text-accent">{properCase(user.username)}</span>
+          <div className='flex flex-col'>
+            <div className='flex items-center gap-x-3'>
+              <span className='text-2xl font-semibold' style={{color: `${user?.profile?.color || '#161616'}`}}>{properCase(user.username)}</span>
               {user?.profile?.Badge?.map((badge, i) => {
                if (badge.icon) return
                return <UserBadge badge={badge} key={`user.${badge.name}.${i}`} />
@@ -62,11 +62,11 @@ const UserCard = ({ showStatics, user }: Props) => {
           </div>
         </div>
         {showStatics && (
-          <div className="flex items-center gap-x-8">
+          <div className='flex items-center gap-x-8'>
             {Array.from({ length: 3 }).map((item, i) => (
-              <div key={i} className="flex flex-col items-center ">
-                <span className="text-2xl font-semibold text-accent">0</span>
-                <span className="">Coment</span>
+              <div key={i} className='flex flex-col items-center '>
+                <span className='text-2xl font-semibold' style={{color: `${user?.profile?.color || '#161616'}`}}>0</span>
+                <span className=''>Coment</span>
               </div>
             ))}
           </div>
