5a4e3b66ccf475ead1ae0723426fbe3fde6b083a
GabrielMar <profissional.gabrielm@gmail.com>
2023-02-21 20:16:25 -0300

    fix: formatting

--- src/components/User/UserCard.tsx
+++ src/components/User/UserCard.tsx
@@ -28,45 +28,69 @@ const UserCard = ({ showStatics, user }: Props) => {
   return (
     <>
       <div
-        className='h-48 rounded-t-md bg-tertiary bg-cover bg-center bg-no-repeat relative'
+        className="relative h-48 rounded-t-md bg-tertiary bg-cover bg-center bg-no-repeat"
         style={{
           backgroundImage: `url(${user?.profile?.background})`,
         }}
       >
-        <span className='left-4 top-4 absolute rounded-md px-2 py-1 text-primary font-medium text-xs' style={{backgroundColor: `${user?.profile?.color || '#161616'}`}}>#{beautyNumber(user?.id)}</span>
-        <div className='absolute right-4 top-4'>
-          <Button secondary Icon={<NotePencil className='mr-3' />} text={t('user_menu_settings')} iconLeft xs/>
+        <span
+          className="absolute left-4 top-4 rounded-md px-2 py-1 text-xs font-medium text-primary"
+          style={{ backgroundColor: `${user?.profile?.color || '#161616'}` }}
+        >
+          #{beautyNumber(user?.id)}
+        </span>
+        <div className="absolute right-4 top-4">
+          <Button
+            secondary
+            Icon={<NotePencil className="mr-3" />}
+            text={t('user_menu_settings')}
+            iconLeft
+            xs
+          />
         </div>
       </div>
-      <div className='relative mb-4 flex h-24 justify-between overflow-hidden rounded-b-md bg-secondary px-8'>
-        <div className='pointer-events-none absolute top-0 left-0 z-[0] h-full w-full opacity-10' style={{backgroundColor: `${user?.profile?.color || '#161616'}`}}/>
-        <div className='z-[1] flex items-center'>
+      <div className="relative mb-4 flex h-24 justify-between overflow-hidden rounded-b-md bg-secondary px-8">
+        <div
+          className="pointer-events-none absolute top-0 left-0 z-[0] h-full w-full opacity-10"
+          style={{ backgroundColor: `${user?.profile?.color || '#161616'}` }}
+        />
+        <div className="z-[1] flex items-center">
           <span
-            className='mr-4 h-20 w-20 rounded-full bg-cover bg-center bg-no-repeat'
+            className="mr-4 h-20 w-20 rounded-full bg-cover bg-center bg-no-repeat"
             style={{ backgroundImage: 'url(/test/avtr.png)' }}
           />
-          <div className='flex flex-col'>
-            <div className='flex items-center gap-x-3'>
-              <span className='text-2xl font-semibold' style={{color: `${user?.profile?.color || '#161616'}`}}>{properCase(user?.username)}</span>
+          <div className="flex flex-col">
+            <div className="flex items-center gap-x-3">
+              <span
+                className="text-2xl font-semibold"
+                style={{ color: `${user?.profile?.color || '#161616'}` }}
+              >
+                {properCase(user?.username)}
+              </span>
               {user?.profile?.Badge?.map((badge, i) => {
-               if (badge.icon) return
-               return <UserBadge badge={badge} key={`user.${badge.name}.${i}`} />
+                if (badge.icon) return
+                return <UserBadge badge={badge} key={`user.${badge.name}.${i}`} />
               })}
             </div>
-            <div className='flex gap-2 mt-1.5'>
-                {user?.profile?.Badge?.map((badge, i) => {
-                  if (!badge.icon) return
-                  return  <UserBadge badge={badge} key={`user.${badge.name}.${i}`} />
+            <div className="mt-1.5 flex gap-2">
+              {user?.profile?.Badge?.map((badge, i) => {
+                if (!badge.icon) return
+                return <UserBadge badge={badge} key={`user.${badge.name}.${i}`} />
               })}
-            </div>  
+            </div>
           </div>
         </div>
         {showStatics && (
-          <div className='flex items-center gap-x-8'>
+          <div className="flex items-center gap-x-8">
             {Array.from({ length: 3 }).map((item, i) => (
-              <div key={i} className='flex flex-col items-center '>
-                <span className='text-2xl font-semibold' style={{color: `${user?.profile?.color || '#161616'}`}}>0</span>
-                <span className=''>Coment</span>
+              <div key={i} className="flex flex-col items-center ">
+                <span
+                  className="text-2xl font-semibold"
+                  style={{ color: `${user?.profile?.color || '#161616'}` }}
+                >
+                  0
+                </span>
+                <span className="">Coment</span>
               </div>
             ))}
           </div>
