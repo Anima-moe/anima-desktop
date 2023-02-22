32d04757b13a5e654065cc89760a70667dd0dc10
Nodge <matheus.silva3@ftc.edu.br>
2023-02-22 05:51:35 -0300

    feat: improve navbar button visibility

--- src/components/Navbar/NavbarIcon.tsx
+++ src/components/Navbar/NavbarIcon.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 function NavbarIcon({ title, Icon, onClick }: Props) {
   return (
-    <div className="flex aspect-square h-full cursor-pointer items-center justify-center rounded-md border border-tertiary border-opacity-50 bg-secondary bg-opacity-60 p-2 text-subtle backdrop-blur-sm duration-300 hover:border-tertiary hover:bg-black hover:text-white active:border-accent active:bg-accent active:text-primary">
+    <div className="flex aspect-square h-full cursor-pointer items-center justify-center rounded-md  border-opacity-50  bg-opacity-60 p-2 backdrop-blur-sm duration-300 hover:border-tertiary hover:bg-black hover:text-white active:border-accent active:bg-accent active:text-primary">
       {Icon}
     </div>
   )
