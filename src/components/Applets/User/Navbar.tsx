3aaf42614f6961b3b10aa99bce146396604ca53d
Nodge <matheus.silva3@ftc.edu.br>
2023-02-22 05:43:46 -0300

    del: remove cog from navbar items

--- src/components/Applets/User/Navbar.tsx
+++ src/components/Applets/User/Navbar.tsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import clsx from 'clsx'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Gear, SignOut, User } from 'phosphor-react'
 
@@ -17,17 +18,15 @@ function Navbar() {
   const router = useRouter()
   const { t } = useTranslation()
 
-  const handleProfile = () => {
-    router.push(`/user/${1}`)
+  const handleLogout = async () => {
+    const { setConfigValue } = await import('@/services/tauri/configValue')
+    const { relaunch } = await import('@tauri-apps/api/process')
+    await setConfigValue('token', '')
+    await relaunch()
   }
-  const handleSettings = () => {
-    router.push(`/user/${1}/edit`)
-  }
-  const handleLogout = () => {}
 
   const menuItems = [
-    { name: t('user_menu_profile'), Icon: User, click: handleProfile },
-    { name: t('user_menu_settings'), Icon: Gear, click: handleSettings },
+    { name: t('user_menu_profile'), Icon: User, href: '/user/me' },
     { name: t('user_menu_signout'), Icon: SignOut, click: handleLogout, class: 'hover:bg-red-400' },
   ]
 
@@ -35,7 +34,7 @@ function Navbar() {
     <div className="">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <button className="flex aspect-square h-full cursor-pointer items-center justify-center rounded-md border border-tertiary border-opacity-50 bg-secondary bg-opacity-60 p-2 text-subtle backdrop-blur-sm duration-300 hover:border-tertiary hover:bg-black hover:text-white active:border-accent active:bg-accent active:text-primary">
+          <button className="flex aspect-square h-full cursor-pointer items-center justify-center rounded-md  border-opacity-50  bg-opacity-60 p-2 backdrop-blur-sm duration-300 hover:border-tertiary hover:bg-black hover:text-white active:border-accent active:bg-accent active:text-primary">
             <User size={22} />
           </button>
         </DropdownMenuTrigger>
@@ -49,18 +48,20 @@ function Navbar() {
               {i === arr.length - 1 && (
                 <DropdownMenuSeparator className="my-2 w-full border border-tertiary" />
               )}
-              <DropdownMenuItem
-                className={clsx(
-                  'cursor-pointer rounded-md bg-secondary px-2 py-2 transition-all duration-200 hover:bg-accent hover:text-primary',
-                  item.class
-                )}
-                onClick={item.click}
-              >
-                <span className="flex">
-                  <item.Icon size={22} className="mr-3" />
-                  {item.name}
-                </span>
-              </DropdownMenuItem>
+              <Link href={item.href || '#'}>
+                <DropdownMenuItem
+                  className={clsx(
+                    'cursor-pointer rounded-md bg-secondary px-2 py-2 transition-all duration-200 hover:bg-accent hover:text-primary',
+                    item.class
+                  )}
+                  onClick={item.click}
+                >
+                  <span className="flex">
+                    <item.Icon size={22} className="mr-3" />
+                    {item.name}
+                  </span>
+                </DropdownMenuItem>
+              </Link>
             </Fragment>
           ))}
         </DropdownMenuContent>
