2774683b0360ce37b54f2666baac28e7716e5d00
Nodge <matheus.silva3@ftc.edu.br>
2023-02-24 00:14:18 -0300

    feat: update playerhead on episode progression

--- src/components/VidstackPlayer/Displays/Endtime.tsx
+++ src/components/VidstackPlayer/Displays/Endtime.tsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+
 
 import dayjs from 'dayjs'
 
+import {User as UserService} from '@/services/anima/user'
 import { useMediaStore } from '@vidstack/react'
 
-function Endtime() {
-  const { currentTime, duration } = useMediaStore()
+function Endtime({episodeId}: {episodeId: number}) {
+  const { currentTime, duration, paused } = useMediaStore()
+  
+  useEffect(()=> {
+    if (Math.round(currentTime) % 5 === 0 && currentTime > 5) {
+      UserService.postEpisodePlayerHead(episodeId, Math.round(duration), Math.round(currentTime))
+    }
+  }, [Math.round(currentTime)])
+
+  useEffect(()=>{
+    if (paused) {
+      UserService.postEpisodePlayerHead(episodeId, Math.round(duration), Math.round(currentTime))
+    }
+  }, [paused])
+
   const { t } = useTranslation()
 
   return (
     <span className="mr-auto flex h-full items-center text-sm opacity-60">
-      <span className="mr-3">•</span>
+      <span className="mx-3">•</span>
       {t('player_endtime', {
         time: dayjs()
           .add(duration - currentTime, 'second')
