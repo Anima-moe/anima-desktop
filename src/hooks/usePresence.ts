7c1f9d16d849dab490c2522674f5631b7a294bd5
Nodge <matheus.silva3@ftc.edu.br>
2023-02-24 04:13:09 -0300

    feat: usePresence hook

---
+++ src/hooks/usePresence.ts
@@ -0,0 +1,45 @@
+import { useTranslation } from 'react-i18next'
+
+import { Dayjs } from 'dayjs'
+
+import { getLocaleMetadata } from '@/services/anima/getMetadataFromMedia'
+
+export default function usePresence() {
+  const { t } = useTranslation()
+
+  async function setPresence(episode: Anima.RAW.Episode, watching: boolean) {
+    console.log('setPresence', episode, watching)
+    if (!episode?.id) {
+      return
+    }
+
+    const { invoke } = await import('@tauri-apps/api')
+
+    await invoke('discord_set_activity', {
+      details: t('activity_details_watching', {
+        episode: getLocaleMetadata<Anima.RAW.Episode, Anima.RAW.EpisodeMetadata>(episode).title,
+        episode_number: episode.number,
+      }),
+      state: watching ? t('activity_watching') : t('activity_browsing'),
+      timestamp: Date.now(),
+      image: 'logo_play',
+    })
+  }
+
+  async function clearPresence(path) {
+    if (!path) {
+      return
+    }
+
+    const { invoke } = await import('@tauri-apps/api')
+
+    await invoke('discord_set_activity', {
+      details: path,
+      state: t('activity_browsing'),
+      timestamp: Date.now(),
+      image: 'logo',
+    })
+  }
+
+  return { setPresence, clearPresence }
+}
