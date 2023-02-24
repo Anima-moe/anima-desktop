10a0b7c9878097eeba16b2f1fbd293310a8d0e94
Nodge <matheus.silva3@ftc.edu.br>
2023-02-24 00:12:35 -0300

    fix: add missing player head swiper component

---
+++ src/components/Episode/PlayerHeadSwiper.tsx
@@ -0,0 +1,40 @@
+import { SkeletonBlock } from 'skeleton-elements/react'
+import SwiperCore, { Virtual, Lazy, Navigation } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/virtual'
+import 'swiper/css/lazy'
+import EpisodePlayerHead from './EpisodePlayerHead'
+
+SwiperCore.use([Virtual, Navigation, Lazy])
+
+type Props = {
+  playerHeads?: Anima.RAW.UserPlayerHead[]
+  loading?: boolean
+}
+
+function SwiperPlayerHead({ playerHeads, loading }: Props) {
+  return (
+    <Swiper
+      modules={[Navigation, Virtual, Lazy]}
+      slidesPerView={5}
+      spaceBetween={12}
+      navigation
+      lazy
+      grabCursor
+      virtual
+    >
+      {playerHeads && (
+        playerHeads.sort((a, b) => b.id - a.id ).map((playerHead, index) => (
+          <SwiperSlide key={playerHead.id} virtualIndex={index} className='mt-4'>
+            <EpisodePlayerHead playerHead={playerHead} />
+          </SwiperSlide>
+        ))
+      )}
+    </Swiper>
+  )
+}
+
+export default SwiperPlayerHead
