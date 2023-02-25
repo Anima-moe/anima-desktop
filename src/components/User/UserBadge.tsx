b6b89c142072419a3145c5666fb7792531709fbf
Nodge <matheus.silva3@ftc.edu.br>
2023-02-25 04:36:08 -0300

    fix: user badges

--- src/components/User/UserBadge.tsx
+++ src/components/User/UserBadge.tsx
@@ -4,12 +4,30 @@ interface IUserBadgeProps {
   badge: Anima.RAW.Badge
 }
 
+const badgeMapping = {
+  'Early Supporter': '/badges/baloon.svg',
+  'Alpha': '/badges/bu_hunter.svg',
+  'Contributor': '/badges/developer.svg',
+  'Test Subject': '/badges/bug_hunter_2.svg',
+  'Donator': '/badges/supporter.svg',
+  'Streak x2': ' /badges/boost1.svg',
+  'Streak x3': ' /badges/boost2.svg',
+  'Streak x4': ' /badges/boost3.svg',
+  'Streak x5': ' /badges/boost4.svg',
+  'Streak x6': ' /badges/boost5.svg',
+  'Streak x7': ' /badges/boost6.svg',
+  'Streak x8': ' /badges/boost7.svg',
+  'Streak x9': ' /badges/boost8.svg',
+  'Streak x10': ' /badges/boost9.svg',
+  'Hero': ' /badges/evemt.svg'
+}
+
 const UserBadge: React.FunctionComponent<IUserBadgeProps> = ({badge}) => {
   return <Tooltip.Provider >
       <Tooltip.Root>
         <Tooltip.Trigger asChild className='cursor-pointer'>
           { badge.icon ? (
-            <img src={`/badges/${badge.icon}.svg`} className='h-6 w-6' />
+            <img src={badgeMapping[badge.name]} className='h-6 w-6' />
           )  : (
             <span className='rounded-md bg-tertiary px-2 py-1 text-xs select-none'>{badge.name}</span>
           ) }
