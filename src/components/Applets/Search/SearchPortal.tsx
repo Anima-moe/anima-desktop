fe6d78d503d93c81869f91ad7283e856300faed0
GabrielMar <profissional.gabrielm@gmail.com>
2023-02-28 05:08:49 -0300

    fix: formatting

--- src/components/Applets/Search/SearchPortal.tsx
+++ src/components/Applets/Search/SearchPortal.tsx
@@ -73,7 +73,7 @@ function SearchPortal({ query = '' }: Props) {
 
   return (
     <div className="fixed left-0 top-0 z-[10] flex h-full w-full flex-col bg-primary bg-opacity-95 px-4 pt-[11rem] backdrop-blur-md">
-      <div className="relative flex h-full w-full max-w-[85rem] mx-auto flex-col">
+      <div className="relative mx-auto flex h-full w-full max-w-[85rem] flex-col">
         {/* DISPLAY AVAILABLE CATEGORIES FOR THIS LOCALE */}
         {categories?.data.length > 0 && (
           <AnimatePresence>
