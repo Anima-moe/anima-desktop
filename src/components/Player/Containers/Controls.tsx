0e8878fa7122a7e718b0536b6261fd9bed70831a
Nodgear <matheus.silva3@ftc.edu.br>
2023-03-07 17:57:12 -0300

    fix: avoids hiding all controls when idle

--- src/components/Player/Containers/Controls.tsx
+++ src/components/Player/Containers/Controls.tsx
@@ -4,7 +4,7 @@ interface IPlayerControlsProps {
 
 const PlayerControls: React.FunctionComponent<IPlayerControlsProps> = (props) => {
   return <div 
-    className='absolute top-0 left-0 w-full h-full flex justify-between flex-col z-[2] media-user-idle:cursor-none overflow-hidden not-media-can-play:bg-primary/60 media-user-idle:bg-transparent not-media-user-idle:bg-primary/40 duration-300 media-buffering:bg-primary/40 media-paused:!bg-primary/60 p-2 media-user-idle:opacity-0 items-center'
+    className='absolute top-0 left-0 w-full h-full flex justify-between flex-col z-[2] media-user-idle:cursor-none overflow-hidden not-media-can-play:bg-primary/60 not-media-user-idle:bg-primary/40 duration-300 media-buffering:bg-primary/40 media-paused:!bg-primary/60 p-2 media-user-idle:bg-transparent items-center'
     >
     {props.children}
   </div>
