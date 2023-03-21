7913febcae30220d583eddbbd235321c8e1f7874
Nodgear <matheus.silva3@ftc.edu.br>
2023-03-21 05:40:55 -0300

    style: mimic animeCard animation for playerHead

--- src/components/Episode/EpisodePlayerHead.tsx
+++ src/components/Episode/EpisodePlayerHead.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { Check } from 'phosphor-react'
 
 import { getLocaleMetadata } from '@/services/anima/getMetadataFromMedia'
+
 type Props = {
   playerHead: Anima.RAW.UserPlayerHead
 }
@@ -11,14 +12,15 @@ type Props = {
 const cardVariants = {
   initial: {
     y: 0,
-    scale: 1,
+    zIndex: 0,
     transition: {
       duration: 0.3,
       ease: 'easeOut',
     },
   },
   hover: {
-    scale: .98,
+    zIndex: 999,
+    y: '-5px',
     transition: {
       duration: 0.3,
       type: 'spring',
@@ -46,6 +48,7 @@ const shadeVariants = {
 
 
 function EpisodePlayerHead({ playerHead }: Props) {
+
   return (
       <motion.div 
         className="flex flex-col cursor-pointer"  
@@ -55,23 +58,23 @@ function EpisodePlayerHead({ playerHead }: Props) {
         <Link href={`/episode/${playerHead?.AnimeEpisode?.id}?seasonid=${playerHead?.AnimeEpisode?.season_id}`}>
           
         <motion.div
-          className="episde-card aspect-video relative w-full rounded-md bg-tertiary bg-cover bg-center overflow-hidden"
+          className="relative w-full overflow-hidden bg-center bg-cover rounded-md episde-card aspect-video bg-tertiary"
           style={{ backgroundImage: `url('${playerHead?.AnimeEpisode?.thumbnail}')` }}
         >
           <motion.div 
           variants={shadeVariants}
           className='pointer-events-none absolute left-0 bottom-0 flex h-full w-full flex-col items-start justify-end overflow-hidden whitespace-nowrap bg-gradient-to-t from-[rgba(16,16,16,.9)] to-transparent px-4 py-2.5 text-left opacity-0'>
-            <motion.div className="flex w-full flex-col justify-center overflow-hidden text-ellipsis">
-              <h2 className="flex text-md font-semibold w-full text-ellipsis overflow-hidden">
+            <motion.div className="flex flex-col justify-center w-full overflow-hidden text-ellipsis">
+              <h2 className="flex w-full overflow-hidden font-semibold text-md text-ellipsis">
                 {getLocaleMetadata<Anima.RAW.Anime, Anima.RAW.AnimeMetadata>(playerHead?.AnimeEpisode?.AnimeSeason?.Anime)?.title ||
                   'Anime title'}
               </h2>
-              <p className="text-sm text-white text-opacity-60 line-clamp-5 group-hover:text-secondary w-full overflow-hidden text-ellipsis relative">
+              <p className="relative w-full overflow-hidden text-sm text-white text-opacity-60 line-clamp-5 group-hover:text-secondary text-ellipsis">
                 S{playerHead.AnimeEpisode.AnimeSeason.number}E{playerHead.AnimeEpisode.number} • {getLocaleMetadata<Anima.RAW.Episode, Anima.RAW.EpisodeMetadata>(playerHead?.AnimeEpisode)?.title ||
                   'Episode title'}
               </p>
               {playerHead.duration - playerHead.head > 90 ? (
-                <div className='w-full h-2 bg-white/20 rounded-sm mt-3 overflow-hidden' >
+                <div className='w-full h-2 mt-3 overflow-hidden rounded-sm bg-white/20' >
                   <div 
                     className='h-full bg-accent'
                     style={{width: `${~~((playerHead.head/playerHead.duration)*100)}%`}} 
