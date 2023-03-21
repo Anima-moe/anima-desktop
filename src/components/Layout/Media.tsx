7e961307991df50685289f2fe593f7581c449e62
Nodgear <matheus.silva3@ftc.edu.br>
2023-03-21 06:11:04 -0300

    style: adjust media container padding

--- src/components/Layout/Media.tsx
+++ src/components/Layout/Media.tsx
@@ -14,7 +14,7 @@ function Media({ children }: Props) {
   const playerModeClasses = clsx({
     'flex bg-primary w-screen h-[calc(100vh-32px)] overflow-y-scroll max-h-[100vh]': true,
     'absolute top-0 h-screen': playerMode === 'expanded',
-    'pt-16': playerMode === 'normal',
+    'pt-10': playerMode === 'normal',
   })
   
   return (
