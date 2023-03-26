70d4025ef27296e639a9ee3bac72798068c79aa1
Nodgear <matheus.silva3@ftc.edu.br>
2023-03-26 03:04:04 -0300

    fix: use window scroll useNavScroll

--- src/hooks/useNavScroll.ts
+++ src/hooks/useNavScroll.ts
@@ -4,22 +4,18 @@ export default function useNavScroll() {
   const [blackHeader, setBlackHeader] = useState(false)
 
   useEffect(() => {
-    const generalContainer = document.getElementById('animacontent') as HTMLDivElement
-
-    if (!generalContainer) return
-
     const scrollListener = () => {
-      if (generalContainer.scrollTop > 60) {
+      if (window.scrollY > 60) {
         setBlackHeader(true)
       } else {
         setBlackHeader(false)
       }
     }
 
-    generalContainer.addEventListener('scroll', scrollListener)
+    window.addEventListener('scroll', scrollListener)
 
     return () => {
-      generalContainer.removeEventListener('scroll', scrollListener)
+      window.removeEventListener('scroll', scrollListener)
     }
   }, [])
 
