0117b07e52cdc4eb871ea9639e1bd3f69eae19ab
Nodgear <matheus.silva3@ftc.edu.br>
2023-03-26 03:02:28 -0300

    fix: add missing user page refactor components

---
+++ src/components/User/ProfileSection.tsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx' 
+
+interface IUserProfileSectionProps {
+  className?: string
+  contentClassName?: string
+  title: string
+  children?: React.ReactNode | React.ReactNode[] | JSX.Element | JSX.Element[]
+  overlayColor?: string
+}
+
+const UserProfileSection: React.FunctionComponent<IUserProfileSectionProps> = ({ className, contentClassName, title, children, overlayColor }) => {
+  const classNames = clsx({ 
+    [className || '']: true , 
+    ' w-full': true 
+  })
+  const contentClassNames = clsx({
+    ' p-4 bg-secondary rounded-b-md flex gap-2': true,
+    [contentClassName || '']: true
+  })
+
+  return <div className={classNames}>
+    <div className='relative flex items-center w-full h-12 px-4 rounded-t-md bg-secondary'>
+      <div className='absolute inset-0 w-full h-full rounded-t-md opacity-5' style={{backgroundColor: overlayColor}} />
+      <h3 className='text-lg text-semibold' >{title}</h3>
+    </div>
+    <div className={contentClassNames}>
+      {children}
+    </div>
+  </div>
+}
+
+export default UserProfileSection
--- src/components/User/UserCard.tsx
+++ src/components/User/UserCard.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next'
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
 import Link from 'next/link'
-import { NotePencil } from 'phosphor-react'
+import { NotePencil, Plus } from 'phosphor-react'
 
 import Button from '@/components/General/Button'
 
@@ -13,6 +13,7 @@ type Props = {
   showStatics?: boolean
   user: Anima.RAW.User
   showEditButton?: boolean
+  showAddConnectionButton?: boolean
 }
 
 dayjs.extend(duration)
@@ -30,111 +31,90 @@ const beautyNumber = (number: number) => {
   return number
 }
 
-const UserCard = ({ showStatics, user, showEditButton }: Props) => {
+const UserCard = ({ showStatics, user, showEditButton, showAddConnectionButton }: Props) => {
   const { t } = useTranslation()
   return (
-    <div className="relative">
+    <div className="relative flex flex-col w-full px-8">
+      {/* BANNER */}
+      <div className="h-[300px] w-full rounded-t-md bg-cover bg-center relative" style={{ backgroundImage: `url('${user?.profile?.banner}')` }}>
+        <div className="absolute px-2 py-1 text-xs rounded-md bg-secondary w-min top-2 left-2">#{beautyNumber(user.id)}</div>
+      </div>
+      {/* AVATAR */}
       <div
-        className="relative mb-8 h-96 select-none rounded-md bg-tertiary bg-cover bg-center bg-no-repeat"
+        className="absolute left-1/2 top-[300px] h-32 w-32  -translate-x-1/2 -translate-y-2/3 rounded-md bg-cover bg-center bg-no-repeat z-[1]"
         style={{
-          backgroundImage: `url(${user?.profile?.banner || '/i/banner.png'})`,
+          backgroundImage: `url('${user?.profile?.avatar ?? 'https://i.imgur.com/CBdQGA3.png'}')`,
         }}
       >
-        <span
-          className="absolute left-4 top-4 rounded-md px-2 py-1 text-xs font-semibold text-primary"
-          style={{ backgroundColor: `${user?.profile?.color || '#8D8D8D'}` }}
-        >
-          #{beautyNumber(user?.id)}
-        </span>
-        <div className="absolute right-4 top-4">
-          {showEditButton && (
+        {user?.profile?.Badge?.map((badge, i) => {
+          if (badge.icon) return
+          return (
+            <UserBadge className="!absolute !bottom-0 !left-1/2 translate-y-1/2 !-translate-x-1/2 z-[2]" badge={badge} key={`user.${badge.name}.${i}`} />
+          )
+        })}
+        {showEditButton && (
             <Link href="/user/me/edit">
               <Button
                 secondary
-                Icon={<NotePencil className="ml-3" />}
-                text={t('user_menu_settings')}
+                Icon={<NotePencil className="" />}
+                // text={t('user_menu_settings')}
+                text=''
+                className="!px-0 !py-0 !rounded-full !w-8 !h-8 bg-primary flex items-center justify-center !absolute !right-0 !top-0 translate-x-1/2 -translate-y-1/2"
+                iconClassName='px-1 py-1'
                 xs
               />
             </Link>
           )}
-        </div>
       </div>
-      <div className="absolute bottom-0 left-0 flex h-24 w-full justify-between overflow-hidden rounded-b-md bg-secondary bg-opacity-80 px-4 backdrop-blur-xl">
-        <div
-          className="pointer-events-none absolute top-0 left-0 z-[0] h-full w-full opacity-10"
-          style={{ backgroundColor: `${user?.profile?.color || '#161616'}` }}
-        />
-        <div className="z-[1] flex items-center">
-          <span
-            className="mr-4 h-20 w-20 rounded-full bg-cover bg-center bg-no-repeat"
-            style={{
-              backgroundImage: `url('${
-                user?.profile?.avatar ?? 'https://i.imgur.com/CBdQGA3.png'
-              }')`,
-            }}
-          />
-          <div className="flex flex-col">
-            <div className="flex items-center gap-x-3">
-              <span
-                className="text-2xl font-semibold"
-                style={{ color: `${user?.profile?.color || '#8D8D8D'}` }}
-              >
-                {user?.username}
-              </span>
-              {user?.profile?.Badge?.map((badge, i) => {
-                if (badge.icon) return
-                return <UserBadge badge={badge} key={`user.${badge.name}.${i}`} />
-              })}
-            </div>
-            <div className="mt-1.5 flex gap-1">
-              {user?.profile?.Badge?.map((badge, i) => {
-                if (!badge.icon) return
-                return <UserBadge badge={badge} key={`user.${badge.name}.${i}`} />
-              })}
-            </div>
-          </div>
+      
+      <div className="flex h-[6.5rem] w-full justify-between rounded-b-md bg-secondary px-8 py-4 relative">
+        <div className='absolute inset-0 w-full h-full opacity-5 z-[0] rounded-b-md' style={{backgroundColor: user.profile.color}} />
+        <div className="flex items-center justify-start w-2/6 gap-3 z-[1]">
+          {showAddConnectionButton && (
+            <Button 
+              text=''
+              Icon={<Plus size={24} />}
+              className='!border-dashed !border-1 !border-subtle text-subtle !p-2 hover:!border-solid hover:!bg-primary hover:text-accent duration-200'
+            />
+          )}
         </div>
-        {showStatics && (
-          <div className="flex items-center gap-x-8">
-            <div className="flex flex-col items-center ">
-              <span
-                className="text-2xl font-semibold"
-                style={{ color: `${user?.profile?.color || '#8D8D8D'}` }}
-              >
-                {user?.profile?.Badge?.length || 0}
-              </span>
-              <span className="">Badges</span>
+        <div className="flex items-end justify-center w-2/6 h-full gap-3 z-[1] pointer-events-none">
+          <h1 className="text-2xl font-bold" style={{ color: `${user?.profile?.color || '#8D8D8D'}` }}>
+            {user.username}
+          </h1>
+        </div>
+        {/* STATS */}
+        <div className="flex items-center justify-end w-2/6 h-full gap-3 z-[1]">
+          {showStatics && (
+            <div className="flex items-center gap-x-8 text-subtle" >
+              <div className="flex flex-col items-center ">
+                <span className="text-2xl font-semibold" style={{ color: `${user?.profile?.color || '#8D8D8D'}` }}>
+                  {user?._count?.Comment || 0}
+                </span>
+                <span className="">{t('user.stats.comments')}</span>
+              </div>
+              <div className="flex flex-col items-center ">
+                <span className="text-2xl font-semibold" style={{ color: `${user?.profile?.color || '#8D8D8D'}` }}>
+                  {user?._count?.UserPlayerHead || 0}
+                </span>
+                <span className="">{t('user.stats.episodes')}</span>
+              </div>
+              <div className="flex flex-col items-center ">
+                <span className="text-2xl font-semibold" style={{ color: `${user?.profile?.color || '#8D8D8D'}` }}>
+                  {dayjs
+                    .duration({
+                      seconds: user?.UserPlayerHead?.reduce((acc, curr) => {
+                        return acc + curr.duration
+                      }, 0),
+                    })
+                    .asHours()
+                    .toFixed(1)} 
+                </span>
+                <span className="">{t('user.stats.hours')}</span>
+              </div>
             </div>
-            <div className="flex flex-col items-center ">
-              <span
-                className="text-2xl font-semibold"
-                style={{ color: `${user?.profile?.color || '#8D8D8D'}` }}
-              >
-                {user?._count?.Comment || 0}
-              </span>
-              <span className="">Coments</span>
-            </div>
-            <div className="flex flex-col items-center ">
-              <span
-                className="text-2xl font-semibold"
-                style={{ color: `${user?.profile?.color || '#8D8D8D'}` }}
-              >
-                {user?._count?.UserPlayerHead || 0}
-              </span>
-              <span className="">Episodes</span>
-            </div>
-            <div className="flex flex-col items-center ">
-              <span
-                className="text-2xl font-semibold"
-                style={{ color: `${user?.profile?.color || '#8D8D8D'}` }}
-              >
-                {dayjs.duration({ seconds: user?.UserPlayerHead?.reduce((acc, curr) => { return acc + curr.head }, 0) }).asHours().toFixed(1)}
-                
-              </span>
-              <span className="">Hours</span>
-            </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   )
