1b9df4ed406880d2ee02670de6ce5d2f085be389
Nodgear <matheus.silva3@ftc.edu.br>
2023-04-09 17:26:25 -0300

    style: tailwind keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,21 @@ module.exports = {
         tertiary: '#141414',
         subtle: '#5D5D5D',
         accent: '#27FF7E',
-      }
+      },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        contentShow: {
+          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+        },
+      },
+      animation: {
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [
