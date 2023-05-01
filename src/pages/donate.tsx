578cdf9b2d543c6cafee30420443ef7bc2a80543
Nodgear <matheus.silva3@ftc.edu.br>
2023-05-01 15:41:14 -0300

    feat: donate page

---
+++ src/pages/donate.tsx
@@ -0,0 +1,12 @@
+import General from '@/components/Layout/General'
+
+interface IDonatePageProps {
+}
+
+const DonatePage: React.FunctionComponent<IDonatePageProps> = (props) => {
+  return <General>
+
+  </General>
+}
+
+export default DonatePage
