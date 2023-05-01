b3eff8fe5d282acee83bc97fcbddef7fe8e600af
Nodgear <matheus.silva3@ftc.edu.br>
2023-05-01 15:36:34 -0300

    fix: add playerhead to EpisodeCard

--- src/components/Episode/EpisodeCard.tsx
+++ src/components/Episode/EpisodeCard.tsx
@@ -1,9 +1,11 @@
+import dayjs from 'dayjs'
 import { t } from 'i18next'
 
 import { getLocaleMetadata } from '@/services/anima/getMetadataFromMedia'
 import * as Tooltip from '@radix-ui/react-tooltip'
 type Props = {
   episode: Anima.RAW.Episode
+  playerHead?: Anima.RAW.UserPlayerHead
 }
 
 const beautyNumber = (number: number) => {
@@ -11,8 +13,8 @@ const beautyNumber = (number: number) => {
   return number
 }
 
-function EpisodeCard({ episode }: Props) {
-  
+function EpisodeCard({ episode, playerHead }: Props) {
+
   return (
     <Tooltip.Provider delayDuration={200}>
     <Tooltip.Root>
@@ -21,12 +23,20 @@ function EpisodeCard({ episode }: Props) {
           className='flex flex-col w-full h-full gap-1 px-2 whitespace-nowrap text-ellipsis'
         >
           <div
-            className={'episode-card aspect-video rounded-md bg-tertiary bg-cover bg-center whitespace-wrap w-full h-full'}
+            className={'episode-card aspect-video rounded-md bg-tertiary bg-cover bg-center whitespace-wrap w-full h-full relative'}
             style={{
               backgroundImage: `url('${episode.thumbnail}')`,
               // width: `calc(calc(80vw - 4rem - 2rem - calc(1rem * ${calculateItemsPerRow(windowSize.width) - 1}))/${calculateItemsPerRow(windowSize.width)})`
             }}
           >
+            {playerHead && (
+              <div className='absolute w-11/12 h-3 overflow-hidden -translate-x-1/2 rounded-sm bottom-2 left-1/2 bg-secondary/50' >
+                <div 
+                  className='h-full rounded-sm bg-accent'
+                  style={{width: `${~~((playerHead.head/playerHead.duration)*100)}%`}} 
+                />
+              </div>
+            )}
           </div>
           <div className='flex flex-col justify-center'>
             <h2 className='flex w-full mb-2 overflow-hidden text-sm text-ellipsis'>
@@ -42,18 +52,27 @@ function EpisodeCard({ episode }: Props) {
         </Tooltip.Trigger>
         <Tooltip.Portal>
           <Tooltip.Content 
-            className="max-w-[350px] bg-tertiary border border-subtle rounded shadow-md p-4 overflow-hidden relative" 
-            side='bottom' 
-            sideOffset={0} 
+            className="max-w-[350px] bg-tertiary border border-subtle/10 rounded shadow-md p-4 overflow-hidden relative" 
+            side='bottom'
+            sideOffset={-50} 
             collisionPadding={10}
           >
-            <div className='absolute inset-0 w-full h-full bg-center bg-cover blur-sm z-[0]' style={{backgroundImage: `url('${episode.thumbnail}')`}} />
-            <div className='absolute inset-0 w-full h-full bg-center bg-cover blur-sm z-[1] from-secondary to-secondary/80 bg-gradient-to-t' />
+
             <span className='z-[2] relative text-white/40 text-sm'>{t('anime.generic.synopsis')}:</span>
-            <p className='text-xs text-white/90 group-hover:text-secondary z-[2] relative mt-2'>
+            <p className='text-xs text-white/90 group-hover:text-secondary z-[2] relative mt-1.5 mb-2'>
               {getLocaleMetadata<Anima.RAW.Episode, Anima.RAW.EpisodeMetadata>(episode)?.synopsis ||
                 'Missing synopsis'}
             </p>
+            {playerHead && <>
+              <span className='z-[2] relative text-white/40 text-sm'>{t('anime.generic.progress')}:</span>
+              <div className='z-[2] relative w-full h-3 rounded-sm bg-white/20 mt-1.5' >
+                <div 
+                  className='relative h-full rounded-sm bg-accent'
+                  style={{width: `${~~((playerHead.head/playerHead.duration)*100)}%`}} 
+                />
+              </div>
+              <span className='relative z-[2] text-xs text-subtle'>{t('generic.date.format', { day: dayjs(playerHead.updated_at).date(), month: dayjs(playerHead.updated_at).month() + 1, year: dayjs(playerHead.updated_at).year() })}</span>
+            </>}
           </Tooltip.Content>
         </Tooltip.Portal>
       </Tooltip.Root>
