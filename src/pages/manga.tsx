e14fe0c9a5ec5530261a2b40b81d223ed9dc6ff9
Nodgear <matheus.silva3@ftc.edu.br>
2023-05-01 15:41:30 -0300

    feat: manga page

---
+++ src/pages/manga.tsx
@@ -0,0 +1,14 @@
+import General from '@/components/Layout/General'
+
+interface IMangaPageProps {
+}
+
+const MangaPage: React.FunctionComponent<IMangaPageProps> = (props) => {
+  return <General>
+    <div className='flex items-center justify-center w-full h-full text-lg font-semibold text-subtle'>
+      Soon™️
+    </div>
+  </General>
+}
+
+export default MangaPage
