9a2fd8ed6e79075eb5225d11cfac2dc13f1b0e11
Nodgear <matheus.silva3@ftc.edu.br>
2023-05-01 15:37:06 -0300

    feat: donate reminder content prop

--- src/components/General/DonationReminder.tsx
+++ src/components/General/DonationReminder.tsx
@@ -3,15 +3,16 @@ import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { X } from 'phosphor-react'
 
-type Props = {}
+type Props = {
+  content?: string
+}
 
-function DonationReminder({}: Props) {
+function DonationReminder({content}: Props) {
   const [isVisible, setIsVisible] = useState(true)
   return isVisible ? (
-    <div className="flex w-full select-none flex-row items-center justify-between rounded-md bg-accent px-4 py-2 text-xs font-semibold text-primary">
+    <div className="flex flex-row items-center justify-between w-full px-4 py-2 text-xs font-semibold rounded-md select-none bg-accent text-primary">
       <span>
-        Hey, listen! @ Anima.moe we will never force ads on you, we survive on donations. Please
-        consider donating to keep the service running ad-free 💞
+        {content || 'Hey, listen! @ Anima.moe we will never force ads on you, we survive on donations. Please consider donating to keep the service running ad-free 💞'}
       </span>
       <X
         size={16}
